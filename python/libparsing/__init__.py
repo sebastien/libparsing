7b1cc95937f1c3bd5beecbb454b7f754283e6866
Sebastien Pierre <sebastien.pierre@gmail.com>
2014-12-24 12:30:01 -0500

    Release 0.4.0

--- python/libparsing/__init__.py
+++ python/libparsing/__init__.py
@@ -18,7 +18,7 @@ try:
 except ImportError:
 	import logging
 
-VERSION            = "0.3.6"
+VERSION            = "0.4.0"
 LICENSE            = "http://ffctn.com/doc/licenses/bsd"
 PACKAGE_PATH       = dirname(abspath(__file__))
 LIB_PATH           = dirname(PACKAGE_PATH)
