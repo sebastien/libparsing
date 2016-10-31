5785b1a6605ad39acd5537703153962fa9618ba9
Sébastien Pierre <sebastien.pierre@gmail.com>
2016-10-31 11:59:54 -0400

    Bumped revision

--- src/h/parsing.h
+++ src/h/parsing.h
@@ -5,7 +5,7 @@
 // License           : BSD License
 // ----------------------------------------------------------------------------
 // Creation date     : 12-Dec-2014
-// Last modification : 10-Feb-2016
+// Last modification : 31-Oct-2016
 // ----------------------------------------------------------------------------
 
 #include <stdlib.h>
@@ -23,7 +23,7 @@
 
 #ifndef __PARSING_H__
 #define __PARSING_H__
-#define __PARSING_VERSION__ "0.7.5"
+#define __PARSING_VERSION__ "0.7.6"
 
 /**
  * #  libparsing
