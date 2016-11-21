4ff744f4cd0f18200e959d08c487c5d9e5985502
Sébastien Pierre <sebastien.pierre@gmail.com>
2016-11-21 11:00:13 -0500

    Release 0.8.11

--- src/python/libparsing/_build.py
+++ src/python/libparsing/_build.py
@@ -10,24 +10,22 @@ ffibuilder = cffi.FFI()
 # In order to avoid _libparsing.so: undefined symbol: PyInt_FromLong
 # SEE: http://community.activestate.com/node/9069
 # SEE: http://cffi.readthedocs.io/en/latest/embedding.html
-ffibuilder.set_source("_libparsing", H_SOURCE + C_SOURCE, extra_link_args=["-Wl,-lpcre,--export-dynamic"])
+ffibuilder.set_source("libparsing._libparsing", H_SOURCE + C_SOURCE, extra_link_args=["-Wl,-lpcre,--export-dynamic"])
 ffibuilder.cdef(FFI_SOURCE)
 
 ffibuilder.embedding_init_code("""
-    from my_plugin import ffi
-
-    @ffi.def_extern()
-    def do_stuff(p):
-        print("adding %d and %d" % (p.x, p.y))
-        return p.x + p.y
+from my_plugin import ffi
+@ffi.def_extern()
+def module_init():
+	pass
 """)
 
-ffibuilder.compile(target="_libparsing.*", verbose=False, tmpdir=join(BASE,"build"))
+ffibuilder.compile(target="src/python/_libparsing.*", verbose=False, tmpdir=join(BASE,"build"))
 
 if __name__ == "__main__":
 	import sys, shutil
 	args = sys.argv[1:]
-	if args:
-		shutil.copy("build/_libparsing.so", args[0])
+	#if args:
+	#	shutil.copy("build/_libparsing.so", args[0])
 
 # EOF - vim: syn=python ts=4 sw=4 noet
